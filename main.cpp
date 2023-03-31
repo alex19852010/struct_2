be7bfdd7b007a6ad8c21b0d4ff02d46edf5aa2e3
Alex Bogushenko <bogushenkoas@ukr.net>
2023-03-31 18:00:31 +0300

    three commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 
-int main() {
-    
+using namespace std;
 
+
+
+struct date
+{
+    int year;
+    int month;
+    int day;
+
+};
+
+struct times
+{
+    int hour;
+    int minute;
+    int second;
+    struct date my_date;
+};
+
+int main() {
+
+date    my_date;
+times   my_time;
+
+
+my_date.year = 1985;
+my_time.my_date.year =1995;
+
+cout << my_date.year << " " << my_time.my_date.year;
 
     return 0;
 }
