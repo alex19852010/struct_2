de2c473b95b8fdf8925fa7e8da45b2241c0d77bb
Alex Bogushenko <bogushenkoas@ukr.net>
2023-04-02 13:30:04 +0300

    six commit

--- main.cpp
+++ main.cpp
@@ -2,36 +2,27 @@
 #include <string>
 using namespace std;
 
+struct flat
+{
+    int area;
+    int height;
+    int room;
+};
 
 int main() {
 
-enum name {alex, fedor, maxim, roman};
+flat my_flat[10];
 
-int choose;
-cout << "choose one of the four names";
-cin >> choose;
+for(int i = 0; i < 10; i ++)
+{
+    my_flat[i].area = i + 10;
+    my_flat[i].height = i + 2;
+    my_flat[i].room = i + 1;
+}
 
-    switch (choose) {
-        case alex:
-         cout << "your name alex";
-         break;
-
-        case fedor:
-            cout << "your name fedor";
-            break;
-
-        case maxim:
-            cout << "your name maxim";
-            break;
-
-        case roman:
-            cout << "your name roman";
-            break;
-
-        default:
-            cout << "not name";
-            break;
-        
+    for(int i = 0; i < 10; i ++)
+    {
+        cout << my_flat[i].room << " " << my_flat[i].area << " " << my_flat[i].height << endl;
     }
     return 0;
 }
