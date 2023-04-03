7a379b414e0d57fa751e3c68de47aebbe76831bd
Alex Bogushenko <bogushenkoas@ukr.net>
2023-04-03 15:56:10 +0300

    8 commit

--- main.cpp
+++ main.cpp
@@ -3,57 +3,48 @@
 #include <vector>
 using namespace std;
 
-struct flat
+struct rooms
 {
-    string name;
-    int area;
-    int height;
+    bool bedroom;
+    bool kitchen;
+    bool bathroom;
+    bool childrenroom;
+    bool livingroom;
+    int square;
 };
+
+struct floors
+{
+    rooms room[4];
+    int ceil_height;
+};
+
+struct houses
+{
+    int total;
+    floors floor[3];
+    int square;
+};
+
+
+struct data
+{
+    int square;
+    int ceil_height;
+    bool available;
+};
+
+struct objects
+{
+    data store;
+    data garage;
+    data bar;
+    houses house;
+};
+
 
 int main() {
 
-flat my_flat[4];
-
-enum number {bedroom , kitchen , bathroom, corridor};
-int type = sizeof(number);
-
-for(int i = 0; i <= type; i ++)
-{
-
-    switch (i)
-    {
-        case bedroom:
-             my_flat[i].area = 30;
-             my_flat[i].name = "bedroom";
-             my_flat[i].height = 3;
-        break;
-
-        case kitchen:
-            my_flat[i].area = 20;
-            my_flat[i].name = "kitchen";
-            my_flat[i].height = 3;
-            break;
-
-        case bathroom:
-            my_flat[i].area = 10;
-            my_flat[i].name = "bathroom";
-            my_flat[i].height = 3;
-            break;
-
-        case corridor:
-            my_flat[i].area = 25;
-            my_flat[i].name = "corridor";
-            my_flat[i].height = 3;
-            break;
-
-
-    }
-
-}
-for(int i = 0; i < 4; i ++)
-{
-    cout << my_flat[i].name << " " << my_flat[i].area << " " << my_flat[i].height << endl;
-}
 
 
 
