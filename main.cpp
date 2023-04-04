d78bffe6b0bad2c88742247d3d02ca649293bed0
Alex Bogushenko <bogushenkoas@ukr.net>
2023-04-04 12:31:54 +0300

    12 commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
 using namespace std;
 
-struct rooms
-{
+struct rooms {
     bool bedroom;
     bool kitchen;
     bool bathroom;
@@ -13,29 +13,25 @@ struct rooms
     int square;
 };
 
-struct floors
-{
+struct floors {
     rooms room[4];
     int ceil_height;
 };
 
-struct houses
-{
+struct houses {
     int total;
     floors floor[3];
     int square;
 };
 
 
-struct data
-{
+struct data {
     int square;
     int ceil_height;
     bool available;
 };
 
-struct objects
-{
+struct objects {
     data store;
     data garage;
     data bar;
@@ -45,10 +41,50 @@ struct objects
 
 int main() {
 
-cout << "hello world";
-cout << "footbal";
+    vector<objects> my_object;
+    objects var;
+    bool available;
+
+    cout << "available of are store,garage and bar on the plots? If yes to enter 1, if not to enter 0" << endl;
+    cout << "available the store:";
+    cin >> var.store.available;
+    cout << "available the garage:";
+    cin >> var.garage.available;
+    cout << "available the bar:";
+    cin >> var.bar.available;
+
+    if (var.store.available) {
+        cout << "enter the store square:";
+        cin >> var.store.square;
+
+    }
 
 
+    if (var.garage.available) {
+        cout << "enter the garage square:";
+        cin >> var.garage.square;
+
+    }
+
+    if (var.bar.available) {
+        cout << "enter the bar square:";
+        cin >> var.bar.square;
+
+    }
+
+    my_object.push_back(var);
+
+    if (var.store.available) {
+        cout << "the bar square: " << my_object[0].store.square << ", ";
+    }
+
+    if (var.garage.available) {
+        cout << "the garage square: " << my_object[0].garage.square << ", ";
+    }
+
+    if (var.bar.available) {
+        cout << "the bar square: " << my_object[0].bar.square << ", ";
+    }
 
     return 0;
 }
